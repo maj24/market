aa11c08cfd055db999aede249db61b40eaf53e95
Maria Jose Patron <mariajose.patron@gmail.com>
2017-02-01 22:41:59 -0600

    Created app seeder

---
+++ database/seeds/AppTablesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AppTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	$tags = factory(Tag::class,5)->create();
+        $sellers = factory(Seller::class, 2)->create();
+        foreach ($sellers as $seller) {
+            $products = factory(Product::class, 3)->create([
+              'seller_id' => $seller->id
+            ])->each(function($product) {
+              $product->tags()->sync(
+                  App\Tag::all()->random(2)
+                );
+            });
+            
+            foreach ($products as $product) {
+              factory(Review::class, 10)->create([
+                'product_id' => $product->id
+              ]);
+            }
+        }
+    }
+}
