e72139201fb72c19436f7da27d9a6779a882fb9d
Maria Jose Patron <mariajose.patron@gmail.com>
2017-03-01 10:58:06 -0600

    Added validation to Reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,14 +3,34 @@ namespace App\Http\Controllers;
 use App\Product;
 use App\Review;
 use Illuminate\Http\Request;
+use Response;
+use Validator;
+use Illuminate\Support\Facades\Input;
+
 class ReviewController extends Controller
 {
     public function store(Request $request, $id) {
-        $attributes = $request->all();
-        $review = Review::create($attributes);
-        $review->product_id = $review->id;
-        $review->save();
-        return response()->json($review);
+        $rules = array(
+            'reviewer_name'	=> 'required',
+            'title' 		=> 'required',
+            'content' 		=> 'required',
+            'date' 			=> 'required'
+        );
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails()) {
+            return Response::json(['message' => 'Faltaron campos']);
+        } else {
+            $review = new Review();
+	        $review->reviewer_name = $request->input('reviewer_name');
+	        $review->title = $request->input('title');
+	        $review->content = $request->input('content');
+	        $review->date = $request->input('date');
+	        $review->product_id = $id;
+	        $review->save();
+
+	        return Response::json($review);
+	    }
     }
     public function index(Request $request, $id) {
         $product = Product::with('reviews')->find($id);
